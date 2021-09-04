0e425acdadb400a3dcd726b9785a6e398fcd6158
Brent Anderson <me@brentjanderson.com>
2021-09-03 21:13:28 -0500

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as Speech from 'expo-speech';
+
+
+interface Letter {
+  char: string;
+  color: string;
+}
+
+const letters: Letter[] = [
+  {char: 'A', color: 'aqua'},
+  {char: 'B', color: 'blue'},
+  {char: 'C', color: 'coral'},
+  {char: 'D', color: 'dodgerblue'},
+  {char: 'E', color: 'chartreuse'},
+  {char: 'F', color: 'fuchsia'},
+  {char: 'G', color: 'gainsboro'},
+  {char: 'H', color: 'hotpink'},
+  {char: 'I', color: 'indigo'},
+  {char: 'J', color: 'forestgreen'},
+  {char: 'K', color: 'khaki'},
+  {char: 'L', color: 'lemonchiffon'},
+  {char: 'M', color: 'mintcream'},
+  {char: 'N', color: 'navy'},
+  {char: 'O', color: 'olive'},
+  {char: 'P', color: 'peachpuff'},
+  {char: 'Q', color: 'goldenrod'},
+  {char: 'R', color: 'rebeccapurple'},
+  {char: 'S', color: 'sienna'},
+  {char: 'T', color: 'thistle'},
+  {char: 'U', color: 'salmon'},
+  {char: 'V', color: 'violet'},
+  {char: 'W', color: 'wheat'},
+  {char: 'X', color: 'slategray'},
+  {char: 'Y', color: 'yellow'},
+  {char: 'Z', color: 'peru'},
+]
+
+function randomLetter() {
+  return letters[Math.floor(Math.random() * letters.length)]
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+  const [letter, setLetter] = useState(randomLetter())
+  Speech.speak(letter.char.toLowerCase(), {voice: "com.apple.ttsbundle.siri_Aaron_en-US_compact", language: 'en-US'});
+  
+  return (<>
+      <TouchableOpacity style={{...styles.container, backgroundColor: letter.color}}  onPress={()=>setLetter(randomLetter())}>
+      <Text style={styles.text}>{letter.char}</Text>
+    </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+      </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 150
+  }
 });
